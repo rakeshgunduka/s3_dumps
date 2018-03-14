0e82e9990a9ef6649f91b96f66ec1f1a66f21c7d
reck31 <rakesh.gunduka@gmail.com>
2018-03-15 02:41:17 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='s3_dumps',
+    packages=['s3_dumps'],
     version=__import__('s3_dumps').__version__,
     description='A package for backup DB and store in s3',
     author='Reck31',
     author_email='rakesh.gunduka@gmail.com',
+    url='https://github.com/rakeshgunduka/s3_dumps',
     include_package_data=True,
-    license=open('LICENSE').read(),
-    packages=['s3_dumps'],
+    license=open('LICENSE.txt').read(),
     install_requires=[
         "boto3"
     ],
@@ -20,15 +21,16 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'postgres_dump=s3_dumps.postgres_dump:main',
-            'redis_dump=s3_dumps.redis_dump:main'
+            'postgres_to_s3=s3_dumps.postgres_to_s3:main',
+            'redis_to_s3=s3_dumps.redis_to_s3:main'
         ],
     },
     scripts=[
-        's3_dumps/postgres_dump.py',
-        's3_dumps/redis_dump.py',
+        's3_dumps/postgres_to_s3.py',
+        's3_dumps/redis_to_s3.py',
         's3_dumps/connect.py',
         's3_dumps/utils.py'
     ],
+    keywords = ['s3', 'backup', 'postgres', 'redis', 'digitaloceanspaces', 'aws'],
     zip_safe=False
 )
